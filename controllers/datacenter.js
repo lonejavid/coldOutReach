184afe658fc506b1789db47bb746fec2bedc4adb
lonejavid <lonejavid0739@gmail.com>
2025-01-02 22:45:41 +0530

    make it better

--- controllers/datacenter.js
+++ controllers/datacenter.js
@@ -50,7 +50,7 @@ const datacenter = async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
-      executablePath: process.env.CHROME_BIN || '/app/.apt/usr/bin/google-chrome-stable',
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/app/.apt/usr/bin/google-chrome-stable',
       headless: true,
       args: [
         '--no-sandbox',
